e3e4b8828ae1f842b661fce891a3ab741d2aa070
Sean Egan <segan@cm-logic.com>
2021-11-23 13:36:55 +0000

    updating comments and return status

--- src/scanoss/threadeddependencies.py
+++ src/scanoss/threadeddependencies.py
@@ -70,12 +70,15 @@ class ThreadedDependencies(ScanossBase):
 
     def run(self, what_to_scan: str = None, wait: bool = True) -> bool:
         """
-
+        Initiate a background scan for the specified file/dir
+        :param what_to_scan: file/folder to scan
+        :param wait: wait for completion
         :return: True if successful, False if error encountered
         """
         what_to_scan = what_to_scan if what_to_scan else self.what_to_scan
         self._errors = False
         try:
+            self.print_msg(f'Searching {what_to_scan} for dependencies...')
             self.inputs.put(what_to_scan)   # Setup an input queue to enable the parent to wait for completion
             self._thread = threading.Thread(target=self.scan_dependencies, daemon=True)
             self._thread.start()
@@ -88,34 +91,35 @@ class ThreadedDependencies(ScanossBase):
 
     def scan_dependencies(self) -> None:
         """
-
+        Scan for dependencies from the given file/dir (from the input queue)
         """
         current_thread = threading.get_ident()
-        self.print_trace(f'Starting worker {current_thread}...')
+        self.print_trace(f'Starting dependency worker {current_thread}...')
         try:
-            what_to_scan = self.inputs.get(timeout=5)                                      # Begin processing the dependency request
+            what_to_scan = self.inputs.get(timeout=5)                # Begin processing the dependency request
             if not self.sc_deps.run_scan(what_to_scan=what_to_scan):
                 self._errors = True
             else:
                 deps = self.sc_deps.produce_from_file()
                 if not deps:
                     self._errors = True
-                else:
+                else:                         # TODO add API call to get dep data
+                    self.print_stderr(f'TODO Need to add gRPC call to get dep data.')
                     self.output.put(deps)
         except Exception as e:
-            ThreadedScanning.print_stderr(f'ERROR: Problem encountered running dependency scan: {e}')
+            self.print_stderr(f'ERROR: Problem encountered running dependency scan: {e}')
             self._errors = True
         finally:
             self.sc_deps.remove_interim_file()
             self.inputs.task_done()
-        self.print_trace(f'Thread complete ({current_thread}).')
+        self.print_trace(f'Dependency thread complete ({current_thread}).')
 
     def complete(self) -> None:
         """
         Wait for input queue to complete processing and complete the worker thread
         """
-        self.inputs.join()
         try:
+            self.inputs.join()
             self._thread.join(timeout=5)
         except Exception as e:
             self.print_stderr(f'WARNING: Issue encountered terminating dependency worker thread: {e}')
