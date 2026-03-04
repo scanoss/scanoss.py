535c41ca57ba1b5abc1402d59be28ef2dc609ccd
eeisegn <eeisegn@gmail.com>
2026-03-04 08:54:11 +0000

    added extra debug

--- src/scanoss/threadedscanning.py
+++ src/scanoss/threadedscanning.py
@@ -144,6 +144,9 @@ class ThreadedScanning(ScanossBase):
         :param wfp: WFP to add to queue
         :param sbom: Per-request SBOM context (optional, overrides global SBOM)
         """
+        if sbom and self.debug:
+            w = (wfp.split('\n'))[0]  # show the first file to help debug context
+            self.print_debug(f'Adding SBOM context: {sbom} to {w} ...')
         if wfp is None or wfp == '':
             self.print_stderr('Warning: empty WFP. Skipping from scan...')
         else:
